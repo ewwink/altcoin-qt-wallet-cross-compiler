2a09eeb6ca83b9801623feea1114f0775cab1599
ewwink <ewwink@yahoo.com>
2018-03-21 06:15:26 +0700

    update

--- build-wallet.sh
+++ build-wallet.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env sh
 
 echo "$WORKDIR"
-cd "$WORKDIR" || echo "eerrrrrorrr"; exit
+cd "$WORKDIR"
 echo "not called"
 # sudo apt-get update -qq -y
 sudo apt-get install p7zip-full autoconf automake autopoint bash bison bzip2 cmake flex gettext git g++ gperf intltool libffi-dev libtool libltdl-dev libssl-dev libxml-parser-perl sudo make openssl patch perl pkg-config python ruby scons sed unzip wget xz-utils
